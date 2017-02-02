bba9ee4f93ed0248168c78b1cc09801c54de4d79
Franco96 <laautarocarranza@hotmail.com>
2017-02-02 16:39:58 -0300

    Commit cambios nuevos iooooo

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -8,6 +8,7 @@ public class RemindersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_reminders);
     }
 }
