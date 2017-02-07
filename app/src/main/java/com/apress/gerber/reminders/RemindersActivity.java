c8a5d1ba36338d4a16f4b402ce225d38705474c1
Franco96 <laautarocarranza@hotmail.com>
2017-02-07 14:56:28 -0300

    add database

--- app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/RemindersActivity.java
@@ -1,27 +1,60 @@
 package com.apress.gerber.reminders;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class RemindersActivity extends AppCompatActivity {
+import static com.apress.gerber.reminders.R.*;
 
-   private ListView  mListView;
+public class RemindersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+          setSupportActionBar(toolbar);
 
-        mListView = (ListView) findViewById(R.id.reminders_list_view);
+        ListView mListView = (ListView) findViewById(id.reminders_list_view);
 
         //The arrayAdatper is the controller in our
         // model-view-controller relationship. (controller)
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.reminders_row, R.id.row_text, new String[]{"first record", "second record", "third record"});
-         mListView.setAdapter(arrayAdapter);    }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), layout.reminders_row, id.row_text, new String[]{"first record", "second record", "third record"});
+        mListView.setAdapter(arrayAdapter);
+    }
     //Remainder of the class listing omitted for brevity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case id.action_new:
+                //create new Reminder
+                Log.d(getLocalClassName(), "create new Reminder");
+                return true;
+            case id.action_exit:
+                finish();
+                return true;
+            default:
+                return false;    }
+
+
 
     }
+
+
+}
 
